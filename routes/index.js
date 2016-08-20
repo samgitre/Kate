c8b48a941750e09c12752a384ece33d0b53bc915
Sam <samgitrepos@gmail.com>
2016-08-20 11:24:40 +0100

    new

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,19 @@ var products = require('../configs/model/products-schema');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-
-  products.find({} ,function (err, docs) {
-    if(typeof products === "undefined"){
+try {
+  products.find({}, function (err, docs) {
+    if (typeof products === "undefined") {
       res.status(404).send('Product collection does not exist');
       return;
     }
     var product = docs;
-    res.render('./shop/index', { title: 'Online Boutique', products: product });
+    res.render('./shop/index', {title: 'Online Boutique', products: product});
   });
+}
+catch (e){
+  document.write(e.message);
+}
 });
 
 module.exports = router;
